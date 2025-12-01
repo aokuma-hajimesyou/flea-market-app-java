76d1aeb7604e860b88c891f0738fe41e4925f164
山中康誠 <aokuma@ip-10-95-3-242.ap-northeast-1.compute.internal>
2025-12-01 14:58:08 +0900

    初回コミット 2025-12/01-14:58

---
+++ src/main/java/com/example/flea_market_app/FleaMarketAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.flea_market_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleaMarketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleaMarketAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flea_market_app/FleaMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flea_market_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleaMarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
